12393981ce2cff2a1268d6878438bcce25816d25
Stéphane Ravet <stephane.ravet.external@stormshield.eu>
2022-05-04 17:02:13 +0200

    Add prettier

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,12 +1,11 @@
-
 class AppController {
-    home(reponse) {
-      reponse.redirect("/product/collection");
-    }
+  home(reponse) {
+    reponse.redirect('/product/collection')
+  }
 
-    error_404(reponse) {
-      reponse.status(404).send('API inexistant');
-    }
+  error_404(reponse) {
+    reponse.status(404).send('API inexistant')
+  }
 }
 
-module.exports = AppController;
+module.exports = AppController
--- routes/app.js
+++ routes/app.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 module.exports = (params) => {
+  const { appController } = params
 
-    const { appController } = params;
+  router.all('/', (requete, reponse) => {
+    appController.home(reponse)
+  })
 
-    router.all('/', (requete, reponse) => {
-        appController.home(reponse);
-    });
+  router.all('/error_404', (requete, reponse) => {
+    appController.error_404(reponse)
+  })
 
-    router.all('/error_404', (requete, reponse) => {
-        appController.error_404(reponse);
-    });
-
-return router;
-
+  return router
 }
